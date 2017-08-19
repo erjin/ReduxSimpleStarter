8d8697a32a2dba5727f7e7d01376fe0379cbaec8
Hao Jin <chrisjin.h@gmail.com>
2017-08-19 02:12:10 -0400

    added a method called onInputChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -7,7 +7,12 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
   render() {
-      return <input/>
+    return <input onChange = {this.onInputChange} />
+    // return <input onChange = { event => console.log(event.target.value)} />
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value)
   }
 }
 
