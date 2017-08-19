b57de3da3e77e9044bfeddfebd09692f9a67853f
Hao Jin <chrisjin.h@gmail.com>
2017-08-19 02:18:37 -0400

    state

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
-//Component = React.Component
+// -- {} means Component = React.Component
 
+
+// -- functional component
 // const SearchBar = () =>{
 //   return <input />;
 // };
 
+// -- class component
 class SearchBar extends Component {
-  render() {
-    return <input onChange = {this.onInputChange} />
-    // return <input onChange = { event => console.log(event.target.value)} />
+  // -- only class has state and constructor
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
   }
 
-  onInputChange(event) {
-    console.log(event.target.value)
+  render() {
+    // -- with separate defined method
+    // return <input onChange = {this.onInputChange} />
+
+    // -- one-line code
+    return <input onChange = { event => console.log(event.target.value)} />
   }
+
+  // onInputChange(event) {
+  //   console.log(event.target.value)
+  // }
 }
 
 export default SearchBar;
